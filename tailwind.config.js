72baa980f945e75abc5db1554df5286b7b56c43c
Shorno <fb.shorno@gmail.com>
2024-10-06 01:22:11 +0600

    fix : configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [daisyui],
+}
+
