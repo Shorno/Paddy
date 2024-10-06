d75a0e9d60150e61cb238b32738a72e1220121de
Shorno <fb.shorno@gmail.com>
2024-10-07 00:04:28 +0600

    display pets by category

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const categoriesAPI = "https://openapi.programming-hero.com/api/peddy/categories"
 const allPetsAPI = "https://openapi.programming-hero.com/api/peddy/pets"
+const singleCategoryPetAPI = "https://openapi.programming-hero.com/api/peddy/category/"
 
 const fetchCategories = async () => {
     const response = await fetch(categoriesAPI);
     const {categories} = await response.json()
     return categories
+}
+const fetchByCategory = async (category) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+    const {data} = await response.json()
+    return data
 }
 
 const fetchAllPets = async () => {
@@ -25,6 +31,7 @@ const displayCategoriesButton = async () => {
         button.innerHTML = ` <img src=${item.category_icon} alt=${item.category}> ${item.category}s`
         button.id = `${item.id}`
         button.type = "button"
+        button.onclick = () => displayPetsByCategory(item.category)
         categoriesDiv.appendChild(button)
     })
 }
@@ -48,11 +55,8 @@ const petCard = (pet) => `
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"/>
                             </svg>
-                            Birth: ${pet.date_of_birth === null || undefined ? "Info missing"
-    :
-    `${new Date(pet.date_of_birth).toLocaleDateString("en-us", {year: "numeric", month: "short", day: "numeric"})}`}
-                            
-                            
+                            Birth: ${pet.date_of_birth === null || undefined ? "Info missing" :
+    `${new Date(pet.date_of_birth).toLocaleDateString("en-us", {year: "numeric", month: "short", day: "numeric"})}`}                           
                             </p>
                         </p>
                         <p class="flex items-center">
@@ -120,9 +124,18 @@ const sortByPrice = async () => {
 }
 
 const sortButton = document.getElementById("sort-button")
-
-
 sortButton.addEventListener("click", sortByPrice)
+
+
+const displayPetsByCategory = async (category) => {
+    const pets = await fetchByCategory(category)
+    const petsListDiv = document.getElementById("pets-list-div")
+    petsListDiv.innerHTML = "";
+
+    pets.map((pet) => {
+        petsListDiv.innerHTML += petCard(pet);
+    })
+}
 
 
 displayAllPets()
